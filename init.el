1e28c8ca4a8b0d739f2ce0ee3a42275acdbdee75
Bert Chang <jrweizhang@gmail.com>
2014-06-14 22:48:33 +0800

    Add basic el-get setup

--- init.el
+++ init.el
@@ -1,6 +1,50 @@
-(require 'package)
-(package-initialize)
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")
-	("melpa" . "http://melpa.milkbox.net/packages/")))
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+;; set local recipes, el-get-sources should only accept PLIST element
+(setq
+ el-get-sources
+ '((:name buffer-move; have to add your own keys
+	  :after (progn
+		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
+		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
+		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
+
+   (:name smex; a better (ido like) M-x
+	  :after (progn
+		   (setq smex-save-file "~/.emacs.d/.smex-items")
+		   (global-set-key (kbd "M-x") 'smex)
+		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
+
+   (:name magit; git meet emacs, and a binding
+	  :after (progn
+		   (global-set-key (kbd "C-x C-z") 'magit-status)))
+   
+   (:name goto-last-change; move pointer back to last change
+	  :after (progn
+		   ;; when using AZERTY keyboard, consider C-x C-_
+		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
+
+(setq packages-utilities
+      '(
+	full-ack
+        ))
+
+(setq packages-theme
+      '(
+        color-theme
+        color-theme-solarized
+        ))
+
+;; install new packages and init already installed packages
+(el-get 'sync
+        packages-utilities
+        packages-theme
+         (loop for src in el-get-sources collect (el-get-source-name src)))
